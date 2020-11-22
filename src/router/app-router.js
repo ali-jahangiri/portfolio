7b65b13517d82364ea7dd6fbddbb2421d46f17bd
ali jahangiri <jahangiri.dev@gmail.com>
2020-11-22 12:49:37 +0330

    publish app & Make HashRouter

--- src/router/app-router.js
+++ src/router/app-router.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 import Home from "../page/home";
 import Header from "../components/header";
 
 const AppRouter = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Header />
     <Switch>
       <Route exact path="/" component={Home} />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default AppRouter;
