6e834c6c6039797bdfa741b7fb27693323077198
ali jahangiri <jahangiri.dev@gmail.com>
2020-11-22 12:45:18 +0330

    Updates

---
+++ precache-manifest.abdaba3766a52d69291a62c78166952a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc359964350d6cf50678",
+    "url": "/portfolio/static/css/main.5b20ad3b.chunk.css"
+  },
+  {
+    "revision": "fc359964350d6cf50678",
+    "url": "/portfolio/static/js/main.55452af9.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "73f90628de2843073359",
+    "url": "/portfolio/static/js/2.60ab1a14.chunk.js"
+  },
+  {
+    "revision": "b6a81fe3034c532b32bfad0ee027d1df",
+    "url": "/portfolio/static/media/nailGif.b6a81fe3.gif"
+  },
+  {
+    "revision": "44c6ea91b5154ef42b672495db2d6ccd",
+    "url": "/portfolio/static/media/ezgif.com-gif-maker.44c6ea91.gif"
+  },
+  {
+    "revision": "69fa45cf1cac3d4bd0249d63ee33fe68",
+    "url": "/portfolio/index.html"
+  }
+];
